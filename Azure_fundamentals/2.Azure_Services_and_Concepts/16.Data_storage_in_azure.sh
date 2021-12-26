018dbefd0258add761949193e4720670a4cca6ac
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-12-27 07:27:35 +0800

    :clown_face: azure manages sql server database :clown_face:

---
+++ Azure_fundamentals/2.Azure_Services_and_Concepts/16.Data_storage_in_azure.sh
@@ -0,0 +1,28 @@
+Modern Applications require data to be available quickly and stored securely and accessbile from all over the world.
+
+Azure provides Storage services for the specific data types
+
+Benefits to Azure data storage solutions
+  1. Automated backup and recovery
+  2. Replication across the world
+  3. Encryptiomn options
+  4. security and platform integration
+
+
+Categories of Data:
+--------------------->
+1. Structured Data(SQL - Relational)
+   -> Azure SQL Database
+   -> Azure Database for MySQL
+   -> Azure Database for PostgreSQL 
+
+2. UnStructured Data
+   -> Azure Blob storage
+   -> Azure File storage 
+   -> Azure Disk Storage 
+
+2. SemiStructured Data(NoSQL)
+   -> Cosmos DB 
+    
+
+
---
+++ Azure_fundamentals/2.Azure_Services_and_Concepts/17.Managed_relational_db_products.sh
@@ -0,0 +1,11 @@
+SQL Server:
+--------------------->
+1. SQL Server on Virtual Machines --> Full Control over the SQL Server.
+2. Azure SQL Database --> Fully managed platform as a service. Always running the latest version of SQL Server.
+3. Azure SQL Managed Instance --> Broadest set of SQL Server capabilities. Benefits of managed platform etc...
+
+4. Azure Database for MySQL
+5. Azure Database for PostgreSQL
+
+scaling, backup&recovery, high availability, Automated patching ad version upgrade etc...
+ 
