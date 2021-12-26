3460828670c012e4af729ed8b1190d36c840cae9
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2021-12-27 06:38:32 +0800

    :camera_flash: azure CDN with profiles ad endpoints :camera_flash:

---
+++ Azure_fundamentals/2.Azure_Services_and_Concepts/14.Windows_virtual_desktop.sh
@@ -0,0 +1,11 @@
+Full Desktop for users
+Apps running remotely
+Similar to Remote Desktop Services.
+
+Multi user session environment
+
+Azure File shares
+
+Azure AD
+Azure MFA 
+
---
+++ Azure_fundamentals/2.Azure_Services_and_Concepts/15.Azure_CDN.sh
@@ -0,0 +1,13 @@
+Azure Content Delivery Network(CDN)
+------------------------------------------->
+Profiles and
+Endpoints
+
+
+DSA(Dynamic Site Acceleration)
+  1. Route optimization
+  2. TCP optimization
+  3. Object Prefecth
+  4. Adaptive Image Compression 
+
+
