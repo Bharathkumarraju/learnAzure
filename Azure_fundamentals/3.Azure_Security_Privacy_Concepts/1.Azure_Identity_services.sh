039a5ce61d31445df5d32e6426abab094b9417e0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-01 09:28:32 +0800

    :tada: Azure AD with users/groups and MFA :tada:

--- Azure_fundamentals/3.Azure_Security_Privacy_Concepts/1.Azure_Identity_services.sh
+++ Azure_fundamentals/3.Azure_Security_Privacy_Concepts/1.Azure_Identity_services.sh
@@ -1,1 +1,36 @@
 # Azure Identity Services
+
+Azure Active Directory
+
+
+Authentication: is like a passport for your own identity
+Authorization: is like visa to enter specific country
+
+In Azure 
+the Authentication: Azure AD 
+the Authorization: Role Based Access Control(RBAC)
+
+
+Azure Active Directory:
+-------------------------------------->
+
+Typically an organization will have one Azure Active Directory tenant that manages the security of their microsoft productsi.e. for each Azure subscription or each office 365 products.
+
+
+Difference between Azure AD and Active Directory Domain Services:
+---------------------------------------------------------------------------------------------------------->
+Azure AD does not provide group policies where as Active Directory Domain Services provides group policies.
+
+
+Azure AD --> Singel sign on and application integration.
+Azure AD Domain Services --> Application migration, NTLM and kerberos support.
+Active Directory Domain Services --> Full directory service, all features offered by active directory.
+
+
+
+Custom Domain Name in Azure AD:
+------------------------------------------------->
+dbkrajuhotmail.onmicrosoft.com
+
+
+Azure AD Single sign-on
