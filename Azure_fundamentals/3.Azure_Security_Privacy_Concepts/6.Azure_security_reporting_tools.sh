5071c1d20359bb87f43d53b68c96b29b5fdd96a6
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-03 07:37:09 +0800

    :ambulance: HIPAA PCI ISO27001 GDPR :ambulance:

--- Azure_fundamentals/3.Azure_Security_Privacy_Concepts/6.Azure_security_reporting_tools.sh
+++ Azure_fundamentals/3.Azure_Security_Privacy_Concepts/6.Azure_security_reporting_tools.sh
@@ -4,3 +4,35 @@ Azure information protection
 Azure security center
 azure key vault 
 
+
+
+Azure Monitor and Service health
+Azure Sentinel
+
+
+Azure information protection(AIP) and security Monitoring tools:
+-------------------------------------------------------------------------->
+Classification --> Metadata
+Protection 
+
+Azure Monitor  --> Metrics, Logs
+Azure Service health
+Azure Advanced threat protection  --> monitor user suspicious activity
+
+
+Azure Key Vault:
+------------------------------------->
+Encryption/Decryption
+Secret Management
+Key Management
+Certificate Management
+
+turn on the soft delete and purge protection
+
+
+Azure Security Center and Azure Sentinel:
+-------------------------------------------------------------------------->
+Protect PaaS
+
+
+Azure Sentinel: Detects threats with artificial intelligence
--- Azure_fundamentals/3.Azure_Security_Privacy_Concepts/7.Azure_Compliance_data_protection.sh
+++ Azure_fundamentals/3.Azure_Security_Privacy_Concepts/7.Azure_Compliance_data_protection.sh
@@ -1,1 +1,18 @@
 # Azure Compliance and data protection
+
+Microsoft privacy statement
+Azure Service trust portal
+
+Regulatory Compliance
+
+HIPAA --> Safeguarding medical information -- Health Insurance Portability and Accountability Act
+PCI --> securing the handling of credicards -- Payment Card Industry standard
+GDPR --> General Data  Protection Regulation 
+FedRAMP
+ISO 27001 --> security standard by ISO 
+
+
+Azure Service Trust Portal 
+Azure Service Trust center 
+
+
