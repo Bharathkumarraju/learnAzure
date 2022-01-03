24ce40018b724f89a035164ad41bf4fea2edbb6e
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-03 15:21:38 +0800

    :tada: https://azure.microsoft.com/en-us/pricing/ :tada:

--- Azure_fundamentals/4.Azure_Pricing_Support_options/1.understand_azure_subscriptions.sh
+++ Azure_fundamentals/4.Azure_Pricing_Support_options/1.understand_azure_subscriptions.sh
@@ -1,3 +1,32 @@
 Subscription  --> Resource Group --> All Azure Resources 
 
+AAD --> Azure Active Directory
 
+Subscriptions <---------------------> Azure Active Directory
+
+Naming a Subscription is important
+
+Company-Department-Location-Environment-Instance
+
+Company: Fabrikam
+Department: Sales, IT, Finance
+Location: use(us east), usw(us west)
+Environment: dev, prod, shared
+Instance: 01, 02, 03, 04
+
+example: fab-fin-usw-prod-01
+example: bharath-ai-sg-test-01
+
+
+Management Groups:
+------------------------------->
+Allows us to apply governance conditions(access & policies) a level of scope above conditions.
+
+
+Management Group
+
+Subscriptions
+
+Resource Groups
+
+Resources
--- Azure_fundamentals/4.Azure_Pricing_Support_options/2.understand_management_of_costs.sh
+++ Azure_fundamentals/4.Azure_Pricing_Support_options/2.understand_management_of_costs.sh
@@ -1,1 +1,34 @@
 # Azure costs 
+Understanding options for purchasing Azure products and services
+
+Free - $200 in Azure credit to be used in 30 Days. Access to free Azure services.
+
+Pay-As-You-Go 
+
+Azure Insights
+
+Factors that affecting Costs:
+----------------------------------->
+1. Location
+2. Resource Type
+3. service --> Enterprise agreement, Direct and CSP 
+4. Egress traffic --> Sending Data into Azure ingress(inbound) is always free. However, pulling data out of Azure Egress(outbound) always has a cost.
+
+
+
+Best practices for minimizing Azure costs:
+--------------------------------------------->
+Reserved Instances
+Quotas
+Azure Hybrid benifit 
+Azure Cost Management
+Spending Limits
+Tags 
+
+
+Pricing Calculator:
+------------------------->
+Azure pricing Calculator
+TCO(Total Cost of Ownership)
+
+https://azure.microsoft.com/en-us/pricing/
