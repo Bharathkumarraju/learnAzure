71116e132b93ce343cc07cc14b2b7cb3935772b9
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-05 05:13:10 +0800

    :globe_with_meridians: azure support plans :globe_with_meridians:

--- Azure_fundamentals/4.Azure_Pricing_Support_options/3.understand_azure_support_options.sh
+++ Azure_fundamentals/4.Azure_Pricing_Support_options/3.understand_azure_support_options.sh
@@ -1,2 +1,15 @@
 # Azure support options
 
+Developer
+Standard
+Professional Direct 
+Premier 
+
+
+MSDN Forum
+
+
+Azure Knowledge Center
+
+
+https://docs.microsoft.com/en-us/answers/products/azure?product=all
