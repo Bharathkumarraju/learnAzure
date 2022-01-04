0172a60f34514d1c90b1883b1a7e4208bd4ce89e
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-05 05:26:24 +0800

    :hammer: public and priavte preview features :hammer:

--- Azure_fundamentals/4.Azure_Pricing_Support_options/4.understand_azure_service_level_agreements.sh
+++ Azure_fundamentals/4.Azure_Pricing_Support_options/4.understand_azure_service_level_agreements.sh
@@ -1,1 +1,15 @@
 # Azure SLA
+SLA for Uptime and Connectivity
+
+Each service will have different levels of availabilty and Connectivity.
+
+
+Azure App Service web app  --> Azure SQL Database 
+
+
+MTTR --> Mean Time To Recover
+MTBF --> Mean Time Between Failures
+
+RTO --> Recovery TIme Objective
+RPO --> Recovery Point Objective 
+
--- Azure_fundamentals/4.Azure_Pricing_Support_options/5.Understand_azure_service_lifecycle.sh
+++ Azure_fundamentals/4.Azure_Pricing_Support_options/5.Understand_azure_service_lifecycle.sh
@@ -1,1 +1,18 @@
 # Azure Service lifecycle
+
+Azure Preview for Public, Private and General Availability
+
+Monitor features and product updates
+
+Azure feature previews
+
+
+Public and Private previews
+
+Public Preview 
+
+
+Private Preview 
+
+
+
