2c744494e789fcfee2a91e6f616a00235f3cfcc1
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-06 06:06:33 +0800

    :bug: how RBAC works :bug:

---
+++ Microsoft_Azure_Administrator/3.Manage_RBAC/1.demo.sh
@@ -0,0 +1,34 @@
+RBAC assings permissions to 
+Users
+Groups
+Applications
+
+and the scope of role assignment can be:
+A subscription
+Resource Group 
+Single specific resource 
+
+Service Principal
+Managed Identity
+
+Broad Roles:
+---------------->
+Owner
+Contributor
+Reader
+User Access Administrator
+Deny assignment
+
+
+RBAC Scope:
+--------------->
+Management Groups
+Subscriptions
+Resource Groups 
+Resources
+
+
+
+
+
+
