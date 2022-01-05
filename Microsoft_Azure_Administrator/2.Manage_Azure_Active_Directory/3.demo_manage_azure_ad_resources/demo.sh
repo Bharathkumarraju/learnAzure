397ffea3056535dec208aeff836f9914106b3e9f
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-06 05:12:21 +0800

    :lipstick: SSPR and Azure AD Conditional Access policies :lipstick:

---
+++ Microsoft_Azure_Administrator/2.Manage_Azure_Active_Directory/3.demo_manage_azure_ad_resources/demo.sh
@@ -0,0 +1,26 @@
+Azure AD Users
+Azure AD Groups
+
+perfoem bulk Azure AD user updates
+
+Microsoft Graph API (instead of ARM API)
+
+https://gist.github.com/ericphan/318fa577e9fa37a6738b4db935d30d29#file-azuread-bulkupdate-userprofile-ps1
+
+
+https://techcommunity.microsoft.com/t5/windows-powershell/bulk-update-azure-ad-with-user-attributes-from-csv/m-p/1374479
+
+
+Self Service Password Reset(SSPR) and Multi-Factor Authentication(MFA)
+
+Azure AD Conditional Access policy to enforce MFA 
+
+
+Manage Multiple AD Tenants:
+---------------------------------->
+
+Transfer Azure subscription
+
+
+
+
