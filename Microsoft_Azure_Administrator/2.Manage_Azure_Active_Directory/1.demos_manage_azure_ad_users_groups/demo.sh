f5d184e5afd6178eb0f7146822239bcb83edca77
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-01-06 04:41:05 +0800

    :rocket: azure Mobile Device Management with Azure AD join :rocket:

---
+++ Microsoft_Azure_Administrator/2.Manage_Azure_Active_Directory/1.demos_manage_azure_ad_users_groups/demo.sh
@@ -0,0 +1,17 @@
+how do you add guest access to azure AD 
+
+MacBook-Pro:external bharathdasaraju$ az ad user create --display-name "bharath" --password "Password@123!" --user-principal-name "bhrth.dsra1@outlook.com"
+The domain portion of the userPrincipalName property is invalid. You must use one of the verified domain names in your organization.
+MacBook-Pro:external bharathdasaraju$
+
+
+Azure AD Groups:
+-------------------------->
+Assigned membership
+Dynamic membership
+Group-assigned roles and licenses
+
+AU --> Administrative Units 
+
+
+
---
+++ Microsoft_Azure_Administrator/2.Manage_Azure_Active_Directory/2.demo_manage_azure_ad_devices/demo.sh
@@ -0,0 +1,32 @@
+Device:
+
+Is a computer system that is capable of registering whether joining an Azure AD tenant and therefore can be managed with azure Mobile Device Management, or MDM Tools.
+
+
+Azure AD join scenario
+
+
+Azure AD Join vs Registration
+
+MDM(Mobile Device Management) --> organization owned device that supports Azure AD device sign-in.
+
+
+Azure AD Registration Vs Azure AD Join:
+----------------------------------------->
+MSA --> MicroSoft Account(MSA)
+
+
+Hybrid Azure AD Join
+SKU(Stock Keeping Unit)
+
+VMs:
+------>
+VM User Login role 
+
+
+Device Management for AAD-Joined Devices:
+------------------------------------------------>
+
+
+
+
