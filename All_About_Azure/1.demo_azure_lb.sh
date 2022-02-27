548ab76d444f1ce67a3fcc1b659f62bca7530b6c
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-02-28 05:16:27 +0800

    :tada: learn azure practically :tada:

---
+++ All_About_Azure/1.demo_azure_lb.sh
@@ -0,0 +1,48 @@
+Front-end configuration
+Hash-based distribution
+Backend pool
+Health probe 
+
+https://timw.info/6f731
+https://docs.microsoft.com/en-us/azure/architecture/reference-architectures/n-tier/multi-region-sql-server
+
+
+Session Affinity:
+
+1. Client IP affinity - Use with RD Gateway
+2. Client IP + protocol - Stateless workloads
+
+NVA --> Network Virtual Appliance 
+
+Floating IP --> Direct Server Return 
+
+
+Service Health
+Resource Health
+Azure Monitor 
+PSPing
+TCPing 
+Network Watcher packer capture 
+
+
+Azure Load Balancer  works on OSI Layer-4 
+Azure Application Gateway works on OSI layer-7
+
+Azure Application Gateway(Web Application Firewall)
+Multi-site routing
+path-based routing 
+
+Multi-tier architecture:
+--------------------------->
+https://docs.microsoft.com/en-us/azure/architecture/reference-architectures/n-tier/multi-region-sql-server
+
+
+Azure Application Gateway:
+------------------------------->
+https://docs.microsoft.com/en-us/azure/application-gateway/overview
+
+
+Traffic Manager:
+--------------------------------->
+
+https://docs.microsoft.com/en-us/azure/traffic-manager/traffic-manager-load-balancing-azure
