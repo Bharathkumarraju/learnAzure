babd8d41e076d8cf1ab3a107ef6bc201a1558310
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-03-05 09:12:57 +0800

    :beers: learn azure for fun :beers:

---
+++ All_About_Azure/3.Azure_Basic_concepts/1.Azure_resource_group.sh
@@ -0,0 +1,15 @@
+A logical container for resources
+
+Development/Test/ProductionResources
+
+Management Groups
+
+Subscriptions -->  Associates accounts, Cost Center
+
+Resource Groups  --> Logical resources, Container
+Examples:
+RG-Project-Dev
+Finance-Resources-rg
+
+
+Resources
