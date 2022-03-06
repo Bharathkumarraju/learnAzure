804c81dacf4cb74d1863be7dbf937adab16e66d3
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-03-06 10:05:19 +0800

    :fire: azure metadata services REST API :fire:

---
+++ All_About_Azure/5.Azure_Compute/demo3.sh
@@ -0,0 +1,18 @@
+Virtual Machine Scale Set
+----------------------------->
+A group of separate VMs sharing the same image.
+
+Managed as a group.
+
+Can be scaled out or in manually according to predefined conditions
+
+Scale Out
+Scale In
+
+
+bharathkumar@Azure:~$ az provider show --namespace microsoft.insights -o table
+Namespace           RegistrationPolicy    RegistrationState
+------------------  --------------------  -------------------
+microsoft.insights  RegistrationRequired  Registering
+bharathkumar@Azure:~$ 
+
---
+++ All_About_Azure/5.Azure_Compute/demo4_azure_metadata.sh
@@ -0,0 +1,11 @@
+It is a REST API that is accessible from the VM.
+
+Providing a lot of info about the machine 
+info includes:
+ 1. SKU
+ 2. storage
+ 3. networking
+ 4. scheduled events
+
+Accessible ONLY from the VM.
+
