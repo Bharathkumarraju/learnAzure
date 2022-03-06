5ad248560e69d340424de9420a9d0355fd9e6c0c
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2022-03-06 13:29:07 +0800

    :beers: publish code :beers:

---
+++ All_About_Azure/5.Azure_Compute/demo5_application.sh
@@ -0,0 +1,8 @@
+ Our app has below services in azure 
+ 
+ including various 
+ compute services (VM, App Services, Azure Kubernetes, Functions), 
+ networking (VNets, NSGs, App Gateway), 
+ data (Azure SQL, Cosmos DB, Storage Account, Redis Cache) and lots more.
+
+ 
