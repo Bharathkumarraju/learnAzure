85b5c70f9fcc860800bb7c2206a18261bd99707c
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2023-06-29 09:26:57 +0800

    remove larze size files

--- All_About_Azure/5.Azure_Compute/demo5_application.sh
+++
@@ -1,8 +0,0 @@
- Our app has below services in azure 
- 
- including various 
- compute services (VM, App Services, Azure Kubernetes, Functions), 
- networking (VNets, NSGs, App Gateway), 
- data (Azure SQL, Cosmos DB, Storage Account, Redis Cache) and lots more.
-
- 
