daac26779e28c85cbe63e0076e378203b10835eb
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2023-07-04 14:08:07 +0800

    relearn azure

---
+++ june29-2023/relearn_azure/1.fundamentals/1.demo.sh
@@ -0,0 +1,14 @@
+cloud computing
+
+On-Demand Self service
+
+Rapid Elasticity
+
+Meausred service
+
+Server Time
+DB Storage
+Function Calls
+
+No need to invest money in non-used resources.
+
---
+++ june29-2023/relearn_azure/1.fundamentals/2.demo.sh
@@ -0,0 +1,8 @@
+CapEx and OpEx
+
+Public Cloud  --> AWS, GCP, AZURE
+Private Cloud --> Azure Stack, Openshift, Vmware cloud
+HYbrid Cloud  --> Azure Arc, AWS Outposts
+
+
+
